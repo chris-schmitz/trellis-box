3c6906e957d2186a7d1af848c6e3990cc64fc737
chris-schmitz <schmitz.chris@gmail.com>
2018-04-22 07:44:47 -0500

    updated the task file to allow me to specify multiple files to copy to trellis

--- code/python/memory/tasks.js
+++ code/python/memory/tasks.js
@@ -1,7 +1,22 @@
 const path = require("path")
 const fs = require("fs")
 
-const trinketPath = path.join("/", "Volumes", "CIRCUITPY", "main.py")
-const mainPyPath = path.join(__dirname, "main.py")
+const trinketPath = path.join("/", "Volumes", "CIRCUITPY")
+const projectPath = path.join(__dirname)
 
-fs.createReadStream(mainPyPath).pipe(fs.createWriteStream(trinketPath))
+const toCopy = [
+    {
+        from: path.join(projectPath, "main.py"),
+        to: path.join(trinketPath, "main.py")
+    },
+    {
+        from: path.join(projectPath, "lib", "pitches.py"),
+        to: path.join(trinketPath, "lib", "pitches.py")
+    }
+]
+
+toCopy.forEach(pathData => {
+    console.log(`Copying: ${pathData.from}`)
+    console.log(`to: ${pathData.to}`)
+    fs.createReadStream(pathData.from).pipe(fs.createWriteStream(pathData.to))
+})
